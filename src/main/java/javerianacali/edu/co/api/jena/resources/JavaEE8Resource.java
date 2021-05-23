a55431a8585537310565cf55b572f8a44e146cde
Yan <yancertuche@gmail.com>
2021-05-23 03:20:02 -0500

    new service

--- src/main/java/javerianacali/edu/co/api/jena/resources/JavaEE8Resource.java
+++ src/main/java/javerianacali/edu/co/api/jena/resources/JavaEE8Resource.java
@@ -18,7 +18,15 @@ import static javerianacali.edu.co.api.query.Query.bulidQuery;
 @Path("response")
 public class JavaEE8Resource {
  
-        static String q ="";
+        static String q ="select (count( distinct ?instance)  as ?cou) ?class " +
+" where {?instance a ?class . ?class a owl:Class} " +
+"group by ?class " +
+"order by ?cou";
+        static String PREFIX =   "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
+                                    "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                                    "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+                                    "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
+                                    "PREFIX uri:<http://www.semanticweb.org/jeank/ontologies/2021/2/untitled-ontology-13#>";
  
     private final ExecutorService executorService = java.util.concurrent.Executors.newCachedThreadPool();
     
@@ -43,4 +51,24 @@ public class JavaEE8Resource {
                .entity(result)
                .build();
     }  
+    
+    @GET
+    @Path("/dona")
+    @Produces("application/json")
+    public void doresDO(@Suspended
+    final AsyncResponse asyncResponse, final String query) {
+        executorService.submit(() -> {
+            asyncResponse.resume(doDoresDO(query));
+        });
+    }  
+
+    private Response doDoresDO(String query) {
+        System.out.println(q);
+        String result = Ontology.GetResultAsString( PREFIX + q);
+        return Response
+                .ok("ok")
+                .header("Access-Control-Allow-Origin", "*")
+                .entity(result)
+                .build();
+    }
 } 
